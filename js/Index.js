8e175acb959a495f087769a878d0bb8184c25ee0
Edy Segura <edysegura@dodson.dreamhost.com>
2017-01-10 19:21:52 -0200

    First commit as baseline

---
+++ js/Index.js
@@ -0,0 +1,78 @@
+﻿/**
+ *
+ * Objeto Literal Index para uso do componente Ajax.js
+ * @author: Edy Segura - edy@segura.pro.br
+ *
+ */
+
+var Index = {
+	
+	init: function() {
+		Index.setLinksAjax();
+		Index.setLinkClear();
+	},
+	
+	
+	setLinksAjax: function() {
+		var links = [
+			document.getElementById('link-content-1'),
+			document.getElementById('link-content-2')
+		];
+		
+		for(var i=0; i<links.length; i++) {
+			links[i].onclick = function() {
+				var urlParams = new QueryString(this.href);
+				
+				Ajax.request({
+					url      : this.href,
+					callback : (urlParams.callback) ? eval('(' + urlParams.callback + ')') : null,
+					update   : true,
+					loading  : true
+				});
+				
+				return false;
+			};
+		}
+		
+	},
+	
+
+	setLinkClear: function() {
+		var link = document.getElementById('link-clear');
+		var div1 = document.getElementById('content-1');
+		var div2 = document.getElementById('content-2');
+		
+		link.onclick = function() {
+			div1.innerHTML = 
+			div2.innerHTML = "";
+			return false;
+		};
+		
+	},
+	
+	
+	showContent: function(response, target) {
+		target.innerHTML = response;
+	},
+	
+	
+	showContent1: function(response) {
+		var target = document.getElementById('content-1');
+		Index.showContent(response, target);
+	},
+	
+	
+	showContent2: function(response) {
+		var target = document.getElementById('content-2');
+		Index.showContent(response, target);
+	},
+	
+	
+	showMsg: function() {
+		alert("Componente Ajax.js");
+	}
+	
+};
+
+//inicializacao
+window.onload = Index.init;
---
+++ js/libs/QueryString.js
@@ -0,0 +1,50 @@
+﻿/**
+ * 
+ * Objeto para parsear querystring, documentacao completa em: 
+ * http://code.google.com/p/jscomponentes/wiki/QueryString
+ * 
+ * QueryString.js
+ * http://jscomponentes.googlecode.com/svn/trunk/QueryString/js/lib/QueryString.js
+ * @author Edy Segura - edy@segura.pro.br
+ * 
+ */
+
+var QueryString = function(url) {
+	
+	//private
+	var start, key, data, name, value;
+	
+	//public
+	this.isOK = false;
+	
+	url = (url) ? url : document.location.toString();
+	
+	//expressão regular para pegar a posição do "?"
+	key  = /\?/i;
+	start = url.search(key);
+	
+	if(start > 1 && url.substring(start + 1).search("=") > 1) {
+		url  = url.substring(start + 1);
+		data = url.split("&");
+		
+		for(var i=0; i<data.length; i++) {
+			name = data[i].split("=");
+			
+			//essa expressão regular é para substituir o sinal "+" por espaço
+			value = decodeURIComponent(name[1].replace(/\+/g, " "));
+			//remove os \r da string
+			value = value.replace(/\r/g, "");
+			//remove os \n da string
+			value = value.replace(/\n/g, " ");
+			
+			try {
+				eval("this." + name[0] + "=\"" + value + "\";");
+			} 
+			catch(oErr) { 
+				alert([oErr.name, oErr.message].join("\n"));
+			}
+		}
+		
+		this.isOK = true;
+	}
+};
